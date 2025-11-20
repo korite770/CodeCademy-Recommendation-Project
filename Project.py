975ff36ed388905351d03536b44c3a0923020b6a
korite770 <korite770@gmail.com>
2025-11-20 18:17:50 +0700

    First Commit

---
+++ Project.py
@@ -0,0 +1,2 @@
+# make a recomemendation on books based on user preference by genres , e.g. fiction, non-fiction, mystery, etc. The program will recommend specific books by title according to their genres
+
